3e66e3d832055c880331338a932545d777c3e91b
Ekrem Hatipoğlu <ekrem.hatipoglu@outlook.com>
2018-03-07 11:20:37 +0300

    behaivors changed

--- controllers/frontend/BaseController.php
+++ controllers/frontend/BaseController.php
@@ -2,9 +2,35 @@
 namespace kouosl\base\controllers\frontend;
 
 use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 class BaseController extends Controller
-{	
+{		
+	
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => \yii\filters\AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                    [
+                        'actions' => ['login', 'signup','error'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
 	public function actions()
 	{
 		return [
